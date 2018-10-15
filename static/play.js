ae8763e13330b531b9dea6d5631c448972eebeb3
Nikita <nikita-reutov@mail.ru>
2018-10-15 17:09:51 +0300

    test

--- static/play.js
+++ static/play.js
@@ -5,7 +5,8 @@ var socket = io.connect('http://' + document.domain + ':' + location.port);
 
 $("#message_form").on('submit', function (e) {
     e.preventDefault();
-    var str = JSON.stringify( $(form).serializeArray());
+    alert('darw');
+    var str = JSON.stringify($(form).serializeArray());
     console.log(str);
     socket.emit('chat_message', str);
 });
